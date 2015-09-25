94c1718620c093922a90e7971144f80476c185fc
JulienRouse <julien.rouse@gmail.com>
2015-09-25 10:53:24 +0200

    knn et csv

---
+++ src/essai/ControllerCSV.java
@@ -0,0 +1,131 @@
+package essai;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import model.TweetSkeleton;
+/**
+ * 
+ * ControllerCSV
+ * 
+ * Handle reading parsing and writing new tweets to a specified csv file.
+ *
+ */
+public class ControllerCSV {
+
+	protected File file;
+	protected Set<Long>  set;
+	protected List<String> listTweet;
+	protected List<String> listNewTweet;
+	
+	
+	public ControllerCSV(File file)
+	{
+		this.file=file;
+		this.set = new HashSet<Long>();
+		this.listTweet = new ArrayList<String>();
+		this.listNewTweet = new ArrayList<String>();
+		readCSV();
+	}
+	
+	/** Open the csv file specified, and reads its content.
+	 * Builds a set of id, and a list of tweets.
+	 * The usecase for the set is to compare the id of new tweet to detect doublon.
+	 * 
+	 */
+	public void readCSV()
+	{
+		BufferedReader br = null;
+		String separator = ";";
+		String line = "";
+		try {
+			br = new BufferedReader(new FileReader(this.file));
+			while ((line = br.readLine()) != null) 
+			{
+				String[] tweet = line.split(separator);
+				set.add(Long.parseLong(tweet[0]));
+				listTweet.add(line);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Check if tweet already are in the database, and if they arent, add them to a list,
+	 * waiting to be written in the csv. 
+	 *  
+	 * @param listNewTweet The list of tweet to add to the csv
+	 */
+	public void addNewTweetToCSV(List<TweetSkeleton> listNewTweetSkeleton)
+	{
+		for (TweetSkeleton elt : listNewTweetSkeleton)
+		{
+			if(!set.contains(elt.getId()))
+			{	
+				set.add(elt.getId());
+				this.listNewTweet.add(elt.toString());
+			}
+			else
+			{
+				System.out.println("DOUBLON--->" + elt.toString());
+			}
+		}
+	}
+	
+	/**
+	 * Write the sorted data to the csv file
+	 */
+	public void writeToCSV()
+	{
+		FileWriter fw = null;
+		try
+		{
+			fw = new FileWriter(this.file, true);
+			 
+			for (String s : this.listNewTweet) {
+				fw.write(s+"\n");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		finally {
+			try {
+				fw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * wrapping up addNewTweetToCSV and writeToCSV
+	 * @param l 
+	 */
+	public void doAll(List<TweetSkeleton> l)
+	{
+		addNewTweetToCSV(l);
+		writeToCSV();
+	}
+	
+}
---
+++ src/essai/KNN.java
@@ -0,0 +1,125 @@
+package essai;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.TweetSkeleton;
+
+public class KNN {
+	
+	public static int distance1 (TweetSkeleton ts1, TweetSkeleton ts2)
+	{
+		String[] words1 = ts1.getText().split(" ");
+		String[] words2 = ts2.getText().split(" ");
+		int motsCommuns = 0;
+		
+		for (String s1 : words1) 
+		{
+			for (String s2 : words2)
+			{
+				if(s1.equals(s2))
+					motsCommuns+=1;
+			}
+		}
+		
+		return (words1.length + words2.length - (motsCommuns*2))/ words1.length;
+	}
+	
+	public static void knn_annotation(TweetSkeleton ts, int neighbours, List<TweetSkeleton> learningDB)
+	{
+		int[] nearest_neighbour = new int[neighbours];
+		int[] nearest_neighbour_distance = new int[neighbours];
+		int max_distance = -1;
+		int indice_max_distance = -1;
+		
+		for(int i=0;i<neighbours;i++)
+		{
+			nearest_neighbour[i] = i;
+			nearest_neighbour_distance[i] = KNN.distance1(ts, learningDB.get(i));
+			
+			if(nearest_neighbour_distance[i]>max_distance)
+			{
+				max_distance = nearest_neighbour_distance[i];
+				indice_max_distance = i;
+			}
+		}
+		
+		for(int i=neighbours;i<learningDB.size();i++)
+		{
+			int d = KNN.distance1(ts, learningDB.get(i));
+			
+			if(d<max_distance)
+			{
+				nearest_neighbour[indice_max_distance] = i;
+				nearest_neighbour_distance[indice_max_distance] = d;
+				indice_max_distance = search_indice_value_max(nearest_neighbour_distance);
+				max_distance = nearest_neighbour_distance[indice_max_distance];
+			}
+		}
+		
+		ts.setAnnotation(vote(nearest_neighbour,learningDB));
+		
+	}
+	
+	/**
+	 * Vote for the annotation to give to a tweet, given his nearest neighbours
+	 * @param nearest_neighbour 
+	 * @return An annotation based on nearest_neighbour
+	 * 
+	 * If there is a tie, we return a neutral annotation
+	 */
+	private static int vote(int[] nearest_neighbour, List<TweetSkeleton> learningDB) {
+		
+		int no_annotation = 0;
+		int negatif = 0;
+		int neutral = 0;
+		int positif = 0;
+		
+		int tmp = -2;
+		for(int i=0;i<nearest_neighbour.length;i++)
+		{
+			tmp = learningDB.get(nearest_neighbour[i]).getAnnotation();
+			if (tmp==-1)
+				no_annotation+=1;
+			else if (tmp == 0)
+				negatif +=1;
+			else if (tmp == 1)
+				neutral +=1;
+			else if (tmp == 2)
+				positif +=1;
+			else
+				System.out.println("PROBLEM ANNOTATION NON CONFORM");
+		}
+		
+		if (negatif>Math.max(neutral,positif))
+			return 0;
+		if (neutral>Math.max(negatif, positif))
+			return 1;
+		if (positif>Math.max(neutral, negatif))
+			return 2;
+		
+		return 1;
+		
+		
+	}
+	
+	
+
+	/**
+	 * Search the index of the max element in the specified array 
+	 * @param tab  An array containing integer
+	 * @return The index of the maximum value found in the tab, -1 if the tab is null
+	 */
+	private static int search_indice_value_max(int[] tab) {
+		if (tab==null)
+			return -1;
+		int max = 0;
+		for(int i=1;i<tab.length;i++)
+		{
+			if (tab[i]>tab[max])
+				max = i;
+		}
+		return max;
+	}
+	
+}
